fffd3044a2941ae22620946f90f0772599bc7fe5
belong <belong.belong@outlook.com>
2017-04-11 22:46:12 +0800

    添加jdk动态代理 ================== 增加cglib动态代理 ================== 添加多线程快排 ================== 添加资源收集项目

---
+++ src/main/java/com/belong/ip/IpToLocation.java
@@ -0,0 +1,29 @@
+package com.belong.ip;
+
+import com.maxmind.geoip.Location;
+import com.maxmind.geoip.LookupService;
+
+import java.net.URL;
+
+/**
+ * Created by belong on 2017/4/11.
+ */
+public class IpToLocation {
+    public static void main(String[] args) {
+        try {
+            // 得到资源目录下的资源文件
+            URL url = IpToLocation.class.getClassLoader().getResource("dat/GeoLiteCity-2013-01-18.dat");
+            LookupService cl = new LookupService(url.getPath(), LookupService.GEOIP_MEMORY_CACHE);
+            Location l2 = cl.getLocation("61.135.169.121");
+            System.out.println(
+                    "countryCode: " + l2.countryCode + "\n" +
+                            "countryName: " + l2.countryName + "\n" +
+                            "region: " + l2.region + "\n" +
+                            "city: " + l2.city + "\n" +
+                            "latitude: " + l2.latitude + "\n" +
+                            "longitude: " + l2.longitude);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/belong/url/URLCrawler.java
@@ -0,0 +1,22 @@
+package com.belong.url;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by belong on 2017/4/11.
+ */
+public class URLCrawler{
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void getURLContent(String str_url){
+        // 用于访问的网址
+        URL url = null;
+        // 用于http的协议连接
+        HttpURLConnection urlConnection = null;
+    }
+
+}
